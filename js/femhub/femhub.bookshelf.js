9456934fc3edebb72855dde5bf1d32e11cf76d34
Mateusz Paprocki <mattpap@gmail.com>
2010-08-20 02:09:13 +0200

    Added support for folders trees

--- js/femhub/femhub.bookshelf.js
+++ js/femhub/femhub.bookshelf.js
@@ -88,64 +88,49 @@ FEMhub.Bookshelf = Ext.extend(Ext.Window, {
     },
 
     initFoldersTree: function() {
-        var root = new Ext.tree.TreeNode({
-            id: 'root-' + FEMhub.unique(),
-            text: 'My Folders',
-        });
+        this.root = new Ext.tree.TreeNode();
 
         this.foldersTree = new Ext.tree.TreePanel({
             region: 'west',
             width: 200,
             split: true,
-            rootVisible: true,
-            root: root,
+            root: this.root,
+            rootVisible: false,
             animate: true,
             enableDD: true,
             containerScroll: true,
         });
 
         this.foldersTree.on('contextmenu', function(node, evt) {
-            var context = new Ext.menu.Menu();
-
-            context.add([
-                new Ext.menu.Item({
-                    text: 'New notebook',
-                    handler: function(node) {
-                        FEMhub.log("new notebook");
-                    },
-                    scope: this,
-                }),
-                new Ext.menu.Item({
+            var context = new Ext.menu.Menu({
+                items: [{
                     text: 'New folder',
-                    handler: function(node) {
-                        FEMhub.log("new folder");
+                    handler: function() {
+                        this.addFolder(node);
                     },
                     scope: this,
-                }),
-                new Ext.menu.Separator(),
-                new Ext.menu.Item({
+                }, '-', {
                     text: 'Rename',
-                    handler: function(node) {
-                        FEMhub.log("rename");
+                    handler: function() {
+                        this.renameFolder(node);
                     },
                     scope: this,
-                }),
-                new Ext.menu.Item({
+                }, {
                     text: 'Delete',
-                    handler: function(node) {
-                        FEMhub.log("delete");
+                    handler: function() {
+                        this.deleteFolder(node);
                     },
                     scope: this,
-                }),
-            ]);
+                }],
+            });
 
             context.showAt(evt.getXY());
             evt.stopEvent();
         }, this);
 
         this.foldersTree.on('click', function(node) {
-            if (/^root-/.test(node.id)) {
-                var id = 'root';
+            if (node.id == this.rootNode.id) {
+                var id = 'root'; // XXX: hack
             } else {
                 var id = node.id;
             }
@@ -172,16 +157,7 @@ FEMhub.Bookshelf = Ext.extend(Ext.Window, {
             });
         }, this);
 
-        FEMhub.RPC.getFolders({}, function(folders) {
-            Ext.each(folders, function(folder) {
-                root.appendChild(new Ext.tree.TreeNode({
-                    id: folder.guid,
-                    text: folder.title,
-                }));
-            });
-
-            root.expand();
-        });
+        this.fillFoldersTree();
     },
 
     initNotebooksGrid: function() {
@@ -268,6 +244,132 @@ FEMhub.Bookshelf = Ext.extend(Ext.Window, {
 
         notebook.show();
         return notebook;
+    },
+
+    isRootNode: function(node) {
+        return node.id == this.rootNode.id;
+    },
+
+    fillFoldersTree: function() {
+        function recFillFoldersTree(node) {
+            FEMhub.RPC.Folders.getFolders({guid: node.id}, function(folders) {
+                Ext.each(folders, function(folder) {
+                    var subNode = new Ext.tree.TreeNode({
+                        id: folder.guid,
+                        text: folder.title,
+                    });
+
+                    node.appendChild(subNode);
+                    recFillFoldersTree(subNode);
+                });
+
+                node.expand();
+            });
+        }
+
+        FEMhub.RPC.Folders.getRoot({}, function(folder) {
+            this.rootNode = new Ext.tree.TreeNode({
+                id: folder.guid,
+                text: folder.title,
+            });
+
+            this.root.appendChild(this.rootNode);
+            recFillFoldersTree(this.rootNode);
+        }, this);
+    },
+
+    isValidFolderName: function(name) {
+        return /^[a-z0-9_][a-z0-9_-]*/i.test(name) && name.length < 100;
+    },
+
+    addFolder: function(node) {
+        Ext.MessageBox.prompt('Add folder', 'Enter folder name:', function(button, title) {
+            if (button === 'ok') {
+                if (this.isValidFolderName(title) === false) {
+                    Ext.MessageBox.show({
+                        title: 'Add folder',
+                        msg: "Invalid folder name.",
+                        buttons: Ext.MessageBox.OK,
+                        icon: Ext.MessageBox.ERROR,
+                    });
+                } else {
+                    FEMhub.RPC.Folders.addFolder({guid: node.id, title: title}, function(result) {
+                        if (result.ok === true) {
+                            node.appendChild(new Ext.tree.TreeNode({
+                                id: result.guid,
+                                text: title,
+                            }));
+
+                            node.expand();
+                        } else {
+                            FEMhub.log("Can't create folder");
+                        }
+                    });
+                }
+            }
+        }, this);
+    },
+
+    renameFolder: function(node) {
+        if (this.isRootNode(node)) {
+            Ext.MessageBox.show({
+                title: 'Rename folder',
+                msg: "Can't rename root node. Sorry. ",
+                buttons: Ext.MessageBox.OK,
+                icon: Ext.MessageBox.ERROR,
+            });
+        } else {
+            Ext.MessageBox.prompt('Rename folder', 'Enter new folder name:', function(button, title) {
+                if (button === 'ok') {
+                    if (this.isValidFolderName(title) === false) {
+                        Ext.MessageBox.show({
+                            title: 'Rename folder',
+                            msg: "Invalid folder name.",
+                            buttons: Ext.MessageBox.OK,
+                            icon: Ext.MessageBox.ERROR,
+                        });
+                    } else {
+                        FEMhub.RPC.Folders.renameFolder({guid: node.id, title: title}, function(result) {
+                            if (result.ok === true) {
+                                node.setText(title);
+                            } else {
+                                FEMhub.log("Can't rename folder");
+                            }
+                        });
+                    }
+                }
+            }, this);
+        }
+    },
+
+    deleteFolder: function(node) {
+        if (this.isRootNode(node)) {
+            Ext.MessageBox.show({
+                title: 'Delete folder',
+                msg: "Can't delete root node. Sorry. ",
+                buttons: Ext.MessageBox.OK,
+                icon: Ext.MessageBox.ERROR,
+            });
+        } else {
+            Ext.MessageBox.show({
+                title: 'Delete folder',
+                msg: 'Do you really want to delete selected folder and all its contents?',
+                buttons: Ext.MessageBox.YESNO,
+                icon: Ext.MessageBox.QUESTION,
+                fn: function(button) {
+                    if (button === 'yes') {
+                        FEMhub.RPC.Folders.deleteFolder({guid: node.id}, function(result) {
+                            if (result.ok === true) {
+                                node.remove(true);
+                            } else {
+                                FEMhub.log("Can't delete folder");
+                            }
+                        });
+                    }
+                },
+                scope: this,
+            });
+        }
     },
 });
 
