c4fa6bd0314c1233421096caa48f6a14f277628a
Mateusz Paprocki <mattpap@gmail.com>
2010-08-25 23:29:36 +0200

    Added FEMhub.Help component

---
+++ js/femhub/femhub.help.js
@@ -0,0 +1,48 @@
+
+FEMhub.Help = Ext.extend(Ext.Window, {
+    constructor: function(config) {
+        config = config || {};
+
+        Ext.apply(config, {
+            title: "FEMhub Help",
+            layout: 'fit',
+            width: 500,
+            height: 400,
+            iconCls: 'femhub-help-icon',
+            bodyCssClass: 'femhub-help-body',
+            closable: true,
+            onEsc: Ext.emptyFn,
+            buttons: [{
+                text: 'Close',
+                handler: function() {
+                    this.close();
+                },
+                scope: this,
+            }],
+        });
+
+        FEMhub.Help.superclass.constructor.call(this, config);
+    },
+
+    onRender: function() {
+        FEMhub.Help.superclass.onRender.apply(this, arguments);
+
+        FEMhub.RPC.Template.render({ template: 'femhub/help.html' }, function(result) {
+            if (result.ok === true) {
+                this.body.createChild({
+                    tag: 'div',
+                    html: result.rendered,
+                });
+            }
+        }, this);
+    },
+});
+
+FEMhub.Modules.Help = Ext.extend(FEMhub.Module, {
+    launcher: {
+        text: 'Help',
+        icon: 'femhub-help-launcher-icon',
+    },
+    winCls: FEMhub.Help,
+});
+
