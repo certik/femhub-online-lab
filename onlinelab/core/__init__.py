2f7d63ccb22f45bc28662449e32edb2ee691d7f6
Mateusz Paprocki <mattpap@gmail.com>
2010-09-25 20:02:17 +0200

    Implemented better management script

--- onlinelab/core/__init__.py
+++ onlinelab/core/__init__.py
@@ -2,3 +2,23 @@
 
 from runtime import main
 
+def init(args):
+    """Initialize a new core server. """
+    raise NotImplementedError("'init' is not implemented yet")
+
+def start(args):
+    """Start an existing core server. """
+    main(args)
+
+def stop(args):
+    """Stop a running core server. """
+    raise NotImplementedError("'stop' is not implemented yet")
+
+def restart(args):
+    """Restart a running core server. """
+    raise NotImplementedError("'restart' is not implemented yet")
+
+def status(args):
+    """Display information about a core server. """
+    raise NotImplementedError("'status' is not implemented yet")
+
--- onlinelab/service/__init__.py
+++ onlinelab/service/__init__.py
@@ -2,3 +2,23 @@
 
 from runtime import main
 
+def init(args):
+    """Initialize a new service. """
+    raise NotImplementedError("'init' is not implemented yet")
+
+def start(args):
+    """Start an existing service. """
+    main(args)
+
+def stop(args):
+    """Stop a running service. """
+    raise NotImplementedError("'stop' is not implemented yet")
+
+def restart(args):
+    """Restart a running service. """
+    raise NotImplementedError("'restart' is not implemented yet")
+
+def status(args):
+    """Display information about a service. """
+    raise NotImplementedError("'status' is not implemented yet")
+
