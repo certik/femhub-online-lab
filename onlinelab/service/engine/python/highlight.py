5c85adb3a6175325771dd91cb8a7b2cfeffe5602
Mateusz Paprocki <mattpap@gmail.com>
2010-10-22 13:51:44 +0200

    Added :func: role to docutils' roles
    It works this way:
    
     :func:`function_name` -> function_name()

--- onlinelab/service/engine/python/highlight.py
+++ onlinelab/service/engine/python/highlight.py
@@ -1,17 +1,26 @@
 """Source code highlighting module based on Pygments. """
 
 import pygments
-import docutils.core
 
 from pygments.lexers import PythonLexer, PythonTracebackLexer
 from pygments.formatters import HtmlFormatter
+
+from docutils import core, nodes
+from docutils.parsers.rst import roles
+
+def func_role(role, rawtext, text, lineno, inliner, options={}, content=[]):
+    """``:func:`` role for docutils (e.g. ``:func:`sin` -> sin()``. """
+    node = nodes.strong(rawtext, text + '()', **options)
+    return [node], []
+
+roles.register_canonical_role('func', func_role)
 
 class Highlight(object):
     """Simple class for highlighting Python. """
 
     def docstring(self, text):
         """Render a Python docstring. """
-        return docutils.core.publish_parts(text, writer_name='html')['fragment']
+        return core.publish_parts(text, writer_name='html')['fragment']
 
     def python(self, code):
         """Highlight a piece of Python source code. """
