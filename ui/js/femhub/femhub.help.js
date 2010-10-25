83c68da2086183d06e975c90540f49934949ee8b
Mateusz Paprocki <mattpap@gmail.com>
2010-10-25 12:02:42 +0200

    Help window gets basic key bindings

--- ui/js/femhub/femhub.help.js
+++ ui/js/femhub/femhub.help.js
@@ -32,6 +32,22 @@ FEMhub.Help = Ext.extend(Ext.Window, {
             }
         }, this);
     },
+
+    getBindings: function() {
+        return FEMhub.Bindings.Help;
+    },
+
+    scrollDownAction: function() {
+        this.body.scroll('b', 40);
+    },
+
+    scrollUpAction: function() {
+        this.body.scroll('t', 40);
+    },
+
+    quitAction: function() {
+        this.close();
+    },
 });
 
 FEMhub.Modules.Help = Ext.extend(FEMhub.Module, {
@@ -40,5 +56,30 @@ FEMhub.Modules.Help = Ext.extend(FEMhub.Module, {
         icon: 'femhub-help-launcher-icon',
     },
     winCls: FEMhub.Help,
+});
+
+Ext.reg('x-femhub-help', FEMhub.Help);
+
+FEMhub.Mappings.Help = Ext.extend(FEMhub.Mapping, {
+    bindings: {
+        scrollDownAction: {
+            specs: [
+                'J -shift -ctrl +alt',
+            ],
+            text: 'Scroll down',
+        },
+        scrollUpAction: {
+            specs: [
+                'K -shift -ctrl +alt',
+            ],
+            text: 'Scroll up',
+        },
+        quitAction: {
+            specs: [
+                'Q +shift -ctrl +alt',
+            ],
+            text: 'Close this window',
+        },
+    },
 });
 
