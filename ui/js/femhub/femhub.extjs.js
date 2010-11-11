0db39e5cd0c0ad43a016207bdcf35ce4eb854c19
Mateusz Paprocki <mattpap@gmail.com>
2010-11-11 16:47:34 +0100

    Added mapping between special keys and their codes

--- ui/js/femhub/femhub.extjs.js
+++ ui/js/femhub/femhub.extjs.js
@@ -5,3 +5,18 @@ Ext.WindowMgr.zseed = 10000;
 
 Ext.Ajax.timeout = null;
 
+// TODO: make this at least a bit cross-browser compatible (huh)
+
+Ext.apply(Ext.EventObjectImpl.prototype, {
+    ';':  59, ':':  59,
+    '=':  61, '+':  61,
+    '-': 109, '_': 109,
+    ',': 188, '<': 188,
+    '.': 190, '>': 190,
+    '/': 191, '?': 191,
+    '`': 192, '~': 192,
+    '[': 219, '{': 219,
+    ']': 221, '}': 221,
+    "'": 222, '"': 222,
+});
+
