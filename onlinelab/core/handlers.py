1f828059a36962defe57090e4e2866b9eb83892e
Mateusz Paprocki <mattpap@gmail.com>
2010-11-19 00:57:50 +0100

    Don't display MeshEditor launcher if not installed

--- onlinelab/core/handlers.py
+++ onlinelab/core/handlers.py
@@ -3,6 +3,7 @@
 import logging
 import smtplib
 import functools
+import traceback
 
 from datetime import datetime
 
@@ -30,16 +31,19 @@ class MainHandler(errors.ErrorMixin, tornado.web.RequestHandler):
     """Render default Online Lab user interface. """
 
     def initialize(self, debug=False):
-        settings = Settings.instance()
-        self.debug = debug or settings.debug
+        self._settings = Settings.instance()
+        self.debug = debug or self._settings.debug
 
     def get(self):
         html = pygments.formatters.HtmlFormatter(nobackground=True)
         css = html.get_style_defs(arg='.highlight')
 
         try:
-            self.render('femhub/desktop.html', debug=self.debug, extra_css=css)
+            self.render('femhub/desktop.html', debug=self.debug,
+                extra_css=css, settings=self._settings)
         except:
+            logging.error("error rendering 'femhub/desktop.html'")
+            logging.error('\n' + traceback.format_exc())
             raise tornado.web.HTTPError(500)
 
 class WebHandler(auth.DjangoMixin, cors.CORSMixin, jsonrpc.APIRequestHandler):
