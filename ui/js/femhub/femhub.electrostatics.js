46c3d0b52ad71ced3f95302d424cfc37d97298fc
Ondrej Certik <ondrej@certik.cz>
2010-11-24 00:48:38 -0800

    Electrostatics application added

---
+++ ui/js/femhub/femhub.electrostatics.js
@@ -0,0 +1,40 @@
+
+FEMhub.Electrostatics = Ext.extend(FEMhub.Window, {
+    constructor: function(config) {
+        config = config || {};
+
+        Ext.apply(config, {
+            title: "Electrostatics",
+            layout: 'fit',
+            width: 885,
+            height: 595,
+            iconCls: 'femhub-mesheditor-icon',
+            bodyCssClass: 'femhub-mesheditor-body',
+            closable: true,
+            onEsc: Ext.emptyFn,
+            items: [{
+                    "title": "Beta Version",
+                    "html": '<b>First try</b>',
+                    flex: 1,
+                }],
+            buttons: [{
+                text: 'Close',
+                handler: function() {
+                    this.close();
+                },
+                scope: this,
+            }],
+        });
+
+        FEMhub.Electrostatics.superclass.constructor.call(this, config);
+    },
+});
+
+FEMhub.Modules.Electrostatics = Ext.extend(FEMhub.Module, {
+    launcher: {
+        text: 'Electrostatics',
+        icon: 'femhub-mesheditor-launcher-icon',
+    },
+    winCls: FEMhub.Electrostatics,
+});
+
